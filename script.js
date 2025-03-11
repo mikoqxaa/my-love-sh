2cd2f1e1edfdd590944f8aab17465907579d9c5b
mikoqxaa <shizalovemepls@gmail.com>
2025-03-11 13:16:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -15,3 +15,22 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 2000);
     }, 200);
 });
+document.addEventListener("DOMContentLoaded", function () {
+    const starsContainer = document.getElementById("stars-container");
+
+    for (let i = 0; i < 100; i++) {
+        let star = document.createElement("div");
+        star.classList.add("star");
+
+        let x = Math.random() * window.innerWidth;
+        let y = Math.random() * window.innerHeight;
+        let size = Math.random() * 3;
+
+        star.style.left = ${x}px;
+        star.style.top = ${y}px;
+        star.style.width = ${size}px;
+        star.style.height = ${size}px;
+
+        starsContainer.appendChild(star);
+    }
+});
